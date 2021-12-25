1b9fc511072b0950b9b515f6be03dc5162b30ac9
Jakob Pupke <jakop@pm.me>
2021-12-25 12:35:16 +0100

    install firefox

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,12 +1,65 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
+let
+  firefox = pkgs.stdenv.mkDerivation rec {
+    pname = "Firefox";
+    version = "95.0.2";
+
+    buildInputs = [ pkgs.undmg ];
+    sourceRoot = ".";
+    phases = [ "unpackPhase" "installPhase" ];
+    installPhase = ''
+      mkdir -p "$out/Applications"
+      cp -r Firefox.app "$out/Applications/Firefox.app"
+    '';
+
+    src = pkgs.fetchurl {
+      name = "Firefox-${version}.dmg";
+      url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/${version}/mac/en-GB/Firefox%20${version}.dmg";
+      sha256 = "sha256-BA/n1zeo6ueE9wzLNy+W3zwdV37gw73odXMw2pYSVdc=";
+    };
+
+    meta = with pkgs.lib; {
+      description = "The Firefox web browser";
+      homepage = "https://www.mozilla.org/en-GB/firefox";
+      platforms = platforms.darwin;
+    };
+  }; in
 
 {
   home.username = "jacke";
   home.homeDirectory = "/Users/jacke";
 
   home.stateVersion = "21.11";
+  home.activation = {
+    copyApplications =
+      let
+        apps = pkgs.buildEnv {
+          name = "home-manager-applications";
+          paths = config.home.packages;
+          pathsToLink = "/Applications";
+        };
+      in
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        baseDir="$HOME/Applications/Home Manager Apps"
+        if [ -d "$baseDir" ]; then
+          rm -rf "$baseDir"
+        fi
+        mkdir -p "$baseDir"
+        for appFile in ${apps}/Applications/*; do
+          target="$baseDir/$(basename "$appFile")"
+          $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+          $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+        done
+      '';
+  };
 
   programs.home-manager.enable = true;
+
+  programs.firefox = {
+    enable = true;
+    package = firefox;
+  };
 
   programs.zsh = {
     enable = true;
