77b3e74b497c3cb924194969274e5552fe6b788d
Jakob Pupke <jakob.pupke@gmail.com>
2022-01-13 11:36:41 +0100

    delete nixpgks folder before symlinking

--- ln.sh
+++ ln.sh
@@ -3,6 +3,8 @@
 mkdir -p ~/.config/karabiner
 mkdir -p ~/.nixpkgs
 
+rm -rf ~/.config/nixpkgs
+
 ln -sFf $(pwd)/nvim ~/.config/nvim
 ln -sFf $(pwd)/nixpkgs ~/.config/nixpkgs
 ln -sf $(pwd)/darwin-configuration.nix ~/.nixpkgs/darwin-configuration.nix
