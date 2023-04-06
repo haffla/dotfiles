4a44c7142c348f5ec2c124dc8dd0a81f0bbebc55
Jakob Pupke <jakop@pm.me>
2023-04-06 09:37:14 +0200

    move home-manager config to new folder, add more casks, some fixes

--- ln.sh
+++ ln.sh
@@ -3,10 +3,10 @@
 mkdir -p ~/.config/karabiner
 mkdir -p ~/.nixpkgs
 
-rm -rf ~/.config/nixpkgs
+rm -rf ~/.config/home-manager
 
 ln -sFf $(pwd)/nvim ~/.config/nvim
-ln -sFf $(pwd)/nixpkgs ~/.config/nixpkgs
+ln -sFf $(pwd)/home-manager ~/.config/home-manager
 ln -sf $(pwd)/.asdfrc ~/.asdfrc
 ln -sf $(pwd)/karabiner.json ~/.config/karabiner/karabiner.json
 ln -sf $(pwd)/git/.gitignore ~/.gitignore
