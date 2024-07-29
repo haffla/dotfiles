b664de650c289ca7a23d9c2021bf33457a736d95
Jakob Pupke <jakop@pm.me>
2024-07-29 21:19:05 +0200

    fix rubocop and install endwise

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters = {
+      rubocop = {
+        command = "bundle exec rubocop",
+      },
+    },
+  },
+}
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "RRethy/nvim-treesitter-endwise" },
+    opts = function(_, opts)
+      opts.endwise = { enable = true }
+      opts.ensure_installed = {
+        "bash",
+        "embedded_template",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "ruby",
+        "tsx",
+        "typescript",
+        "yaml",
+      }
+    end,
+  },
+}
