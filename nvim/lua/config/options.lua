2c4be30e4988a8515168f60325b746bd388a882e
Jakob Pupke <jakop@pm.me>
2024-09-13 13:11:40 +0200

    ruby lsp and stuff

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,5 +4,5 @@
 --
 --
 -- http://www.lazyvim.org/extras/lang/ruby#nvim-lspconfig
-vim.g.lazyvim_ruby_lsp = "solargraph"
+vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "rubocop"
